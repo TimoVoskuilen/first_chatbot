50ccd921acf2bf23d8d8bb7c4cdd9568fe2b8f2e
timovoskuilen <timovoskuilen@gmail.com>
2026-01-06 21:07:19 +0100

    feat initialized an alternative BPE style tokenizer

--- src/bigram.py
+++ src/bigram.py
@@ -75,7 +75,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))
         x = tok_emb + pos_emb
-        logits = self.lm_head(tok_emb) # (B, T, vocab_size)
+        logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
             loss = None
